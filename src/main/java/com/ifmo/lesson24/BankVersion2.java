0b93d8b82f1c60109a8b9a1b6de646622d225a6e
Pivansm <pivansm@outlook.com>
2019-12-15 02:57:50 +0300

    Java less 24 Bank and BankVersion2 updt1

--- src/main/java/com/ifmo/lesson24/BankVersion2.java
+++ src/main/java/com/ifmo/lesson24/BankVersion2.java
@@ -6,12 +6,12 @@ import java.util.List;
 import java.util.Map;
 import java.util.Random;
 import java.util.concurrent.*;
-import java.util.concurrent.locks.*;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
 
-public class Bank {
-
-    private Map<Long, User> users = new ConcurrentHashMap<>();
-    private List<Account> accounts = new CopyOnWriteArrayList<>();
+public class BankVersion2 {
+    private Map<Long, BankVersion2.User> users = new ConcurrentHashMap<>();
+    private List<BankVersion2.Account> accounts = new CopyOnWriteArrayList<>();
 
     private static class User {
         private final long id;
@@ -64,8 +64,6 @@ public class Bank {
 
     public static Object object = new Object();
     private static List<String> NAME= List.of("Ivan", "Semen", "John", "Elena", "Alex", "Vasya");
-    //private static ExecutorService pool = Executors.newFixedThreadPool(1);
-    //private static ExecutorService pool = Executors.newSingleThreadExecutor();
     private static Lock lock = new ReentrantLock();
     private static boolean flag = false;
 
@@ -75,59 +73,47 @@ public class Bank {
         // Другими словами, создайте 100 потоков или пул из 100 потоков, в которых
         // выполните перевод вызовом метода transferMoney().
 
-        ExecutorService pool = Executors.newFixedThreadPool(8);
+        ExecutorService pool = Executors.newFixedThreadPool(100);
 
         Random rnd = new Random();
-        Bank bank = new Bank();
+        BankVersion2 bank = new BankVersion2();
         //Bank.User user = null;
 
         //Генерация User
         for (int i = 0; i < 100; i++) {
-            Bank.User user = new Bank.User(i, NAME.get(rnd.nextInt(NAME.size())));
-            Bank.Account account = new Bank.Account(i, user.id, rnd.nextInt(1000));
+            BankVersion2.User user = new BankVersion2.User(i, NAME.get(rnd.nextInt(NAME.size())));
+            BankVersion2.Account account = new BankVersion2.Account(i, user.id, rnd.nextInt(1000));
             bank.users.put((long) i, user);
             bank.accounts.add(account);
         }
+        //System.out.println("" + bank.accounts);
 
-        System.out.println("" + bank.accounts);
-
-       for (int i = 0; i < 100; i++) {
-            pool.submit(() -> {
-                System.out.println("--" + Thread.currentThread().getName());
-                Account accountFrom = bank.accounts.get(rnd.nextInt(100));
-                Account accountTo = bank.accounts.get(rnd.nextInt(100));
-                transferMoney(accountFrom, accountTo, rnd.nextInt(1000));
-              });
-        }
-
-        /*List<Future<Bank.Transaction>> futures = new ArrayList<>(100);
+        List<Future<BankVersion2.Transaction>> futures = new ArrayList<>(100);
         for(int i = 0; i < 100; i++) {
-            Bank.Account accountFrom = bank.accounts.get(rnd.nextInt(100));
-            Bank.Account accountTo = bank.accounts.get(rnd.nextInt(100));
-            Future<Bank.Transaction> future = pool.submit(new TransLogger(accountFrom, accountTo, rnd.nextInt(1000)));
+            BankVersion2.Account accountFrom = bank.accounts.get(rnd.nextInt(100));
+            BankVersion2.Account accountTo = bank.accounts.get(rnd.nextInt(100));
+            //Future<Bank.Transaction> future = pool.submit(() -> transferMoney(accountFrom, accountTo, 100));
+            Future<BankVersion2.Transaction> future = pool.submit(new BankVersion2.TransLogger(accountFrom, accountTo, rnd.nextInt(1000)));
             futures.add(future);
         }
-
-        for (Future<Bank.Transaction> future : futures) {
-            Bank.Transaction account = future.get();
-            System.out.println("" + account.toString());
-        }*/
-
-        //System.out.println("" + counts.toString());
         pool.shutdown();
+        for (Future<BankVersion2.Transaction> future : futures) {
+            BankVersion2.Transaction account = future.get();
+            System.out.println("" + account.toString());
+        }
+        //pool.shutdown();
     }
 
     // TODO Самая главная часть работы!
-    public static void transferMoney(Account from, Account to, long amount) {
+    public static void transferMoney(BankVersion2.Account from, BankVersion2.Account to, long amount) {
         // 1. Атомарно и потокобезопасно перевести деньги в количестве 'amount' со счёта 'from' на счёт 'to'.
         // 2. Создать объект Transaction, содержащий информацию об операции и отправить в очередь
         // потоку Logger, который проснётся и напечатает её.
         Logger logger;
 
         //boolean flag = false;
-        //synchronized(Bank.Account.class) {
         synchronized(Bank.class) {
-        //synchronized(object) {
+            //synchronized(object) {
             if (from.amount - amount < 0) {
                 flag = false;
                 System.out.println("Acc from: " + from.id + " acc to " + to.id + " flag  " + flag);
@@ -139,40 +125,33 @@ public class Bank {
                 flag = true;
             }
 
-            //Транзакция
-            lock.lock();
-              Bank.Transaction tran = new Bank.Transaction(from.id, to.id, amount, flag);
-              logger = new Logger(tran);
-              new Thread(logger, "logger").start();
-            lock.unlock();
-         }
+        }
 
     }
 
     private static class Logger implements Runnable {
-        Bank.Transaction tran;
+        BankVersion2.Transaction tran;
 
-        public Logger(Bank.Transaction tran) {
+        public Logger(BankVersion2.Transaction tran) {
             this.tran = tran;
         }
 
         @Override
         public void run() {
             System.out.println("Logger проснулся " + Thread.currentThread().getName());
-              lock.lock();
-                  System.out.println("" + tran.toString());
-              lock.unlock();
+            lock.lock();
+            System.out.println("" + tran.toString());
+            lock.unlock();
 
         }
     }
 
-    private static class TransLogger implements Callable<Bank.Transaction> {
-        //private boolean flag;
-        Account from;
-        Account to;
+    private static class TransLogger implements Callable<BankVersion2.Transaction> {
+        BankVersion2.Account from;
+        BankVersion2.Account to;
         long amount;
 
-        public TransLogger(Account from, Account to, long amount) {
+        public TransLogger(BankVersion2.Account from, BankVersion2.Account to, long amount) {
             this.from = from;
             this.to = to;
             this.amount = amount;
@@ -180,10 +159,10 @@ public class Bank {
         }
 
         @Override
-        public Transaction call() throws Exception {
+        public BankVersion2.Transaction call() throws Exception {
 
             transferMoney(from, to, amount);
-            Bank.Transaction tran = new Bank.Transaction(from.id, to.id, amount, flag);
+            BankVersion2.Transaction tran = new BankVersion2.Transaction(from.id, to.id, amount, flag);
             return tran;
         }
     }
